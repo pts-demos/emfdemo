23f34f86d13aa88e5d9955d18f59f9323bb7cc45
Jere Nevalainen <jere.nevalainen@iki.fi>
2018-09-01 12:28:49 +0100

    Basic pulsating effect

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,5 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 90, window.innerWidth/window.innerHeight, 0.1, 1000 );
-//camera.rotateZ(90);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -10,6 +9,7 @@ var basicMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 
 var pivot = new THREE.Object3D();
 
+var waveVertices = [];
 emflogo.objects[0].vertices.forEach(function(vertex){
     var geometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );
     var material = basicMaterial
@@ -17,12 +17,13 @@ emflogo.objects[0].vertices.forEach(function(vertex){
     cube.position.x = vertex[0]*2;
     cube.position.y = vertex[1]*2;
     cube.position.z = vertex[2]*2;
+    waveVertices.push(cube);
     pivot.add(cube);
 });
 pivot.rotateZ(Math.PI/4);
-//pivot.position.x = -6;
 scene.add( pivot );
 
+var ringVertices = [];
 var pivotRing = new THREE.Object3D();
 emflogo.objects[1].vertices.forEach(function(vertex){
     var geometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );
@@ -31,6 +32,7 @@ emflogo.objects[1].vertices.forEach(function(vertex){
     cube.position.x = vertex[0]*4;
     cube.position.y = vertex[1]*4;
     cube.position.z = vertex[2]*4;
+    ringVertices.push(cube);
     pivotRing.add(cube);
 });
 scene.add(pivotRing);
@@ -39,11 +41,31 @@ camera.position.x = 2;
 camera.position.y = 1;
 camera.position.z = 25;
 
+var originalScale = ringVertices[0].position.x;
+
+var scaleThings = function(scale) {
+    ringVertices.forEach(function(vertex){
+        vertex.position.x *= scale;
+        vertex.position.y *= scale;
+        vertex.position.z *= scale;
+    });
+    waveVertices.forEach(function(vertex){
+        vertex.position.x *= scale;
+        vertex.position.y *= scale;
+        vertex.position.z *= scale;
+    });
+};
+
+
 var animate = function () {
     requestAnimationFrame( animate );
 
+    if(ringVertices[0].position.x <= originalScale){
+        scaleThings(1.5);
+    } else {
+        scaleThings(0.97);
+    }
     pivot.rotation.y -= 0.1;
-    //pivotRing.rotation.z += 0.7;
     pivotRing.rotation.y += 0.05;
 
     renderer.render( scene, camera );
